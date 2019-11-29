cfd383ee33599de7bf44c92a175f34f422a43705
TheCodeBite <kamh.1996.hmak@gmail.com>
2019-11-29 00:40:36 -0600

    proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/index/index.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+
+  constructor() { }
+  buscar = false;
+
+  ngOnInit() {
+  }
+
+  search(){
+    this.buscar = true;
+  }
+
+}
